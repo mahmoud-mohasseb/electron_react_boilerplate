b12b76de48a11f8e3761d80555ec4714c76c0f7a
Mahmoud Mohasseb <66387837+mahmoud-mohasseb@users.noreply.github.com>
2021-09-18 21:08:18 +0300

    Add files via upload

---
+++ src/pages/Ayan.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Ayan = () => {
+  return (
+    <div>
+      <h1>Ayan page</h1>
+      <p>wondering why doen't work properly from the first</p>
+      <p>wondering why doen't work properly from the first</p>
+      <p>wondering why doen't work properly from the first</p>
+      <p>wondering why doen't work properly from the first</p>
+      <img
+        src='https://e7.pngegg.com/pngimages/809/515/png-clipart-song-youtube-rajja-rani-youtube-arm-girl.png'
+        alt=''></img>
+    </div>
+  );
+};
+
+export default Ayan;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <img
+        src='https://e7.pngegg.com/pngimages/809/515/png-clipart-song-youtube-rajja-rani-youtube-arm-girl.png'
+        alt=''></img>
+    </div>
+  );
+};
+
+export default Home;
